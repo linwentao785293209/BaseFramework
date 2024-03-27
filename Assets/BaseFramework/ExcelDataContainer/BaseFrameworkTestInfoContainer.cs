e1f3669639e94baa3d855b5c65f3caf5c66db4e5
linwentao785293209 <785293209@qq.com>
2024-03-28 01:38:48 +0800

    添加测试Excel并生成

---
+++ Assets/BaseFramework/ExcelDataContainer/BaseFrameworkTestInfoContainer.cs
@@ -0,0 +1,5 @@
+using System.Collections.Generic;
+public class BaseFrameworkTestInfoContainer
+{
+    public Dictionary<int, BaseFrameworkTestInfo>dataDic = new Dictionary<int, BaseFrameworkTestInfo>();
+}
